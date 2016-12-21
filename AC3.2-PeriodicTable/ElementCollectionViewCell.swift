ebcea7e9f0c71cb40d315c99865ac6680bbb32b0
Erica Y Stevens <ericastevens99@gmail.com>
2016-12-21 10:51:56 -0500

    Added Xcode project

---
+++ AC3.2-PeriodicTable/ElementCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ElementCollectionViewCell.swift
+//  AC3.2-PeriodicTable
+//
+//  Created by Erica Y Stevens on 12/21/16.
+//  Copyright © 2016 Erica Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ElementCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
